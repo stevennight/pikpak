ff04be8183919e8e09502b52e17d871e7293a5d6
stevennight <stevennight@qq.com>
2022-08-06 23:30:19 +0800

    update proxy config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 export const proxy = [
   'https://api.13pikpak.cf',
-  'https://api.14pikpak.cf',
+  // 'https://api.14pikpak.cf',
   'https://api.15pikpak.cf',
   'https://api.16pikpak.cf',
-  'https://api.17pikpak.cf',
+  // 'https://api.17pikpak.cf',
   'https://api.18pikpak.cf',
 ]
 
